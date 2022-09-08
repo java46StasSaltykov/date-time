b2a6c7dc7daafa0e66a956c584580e3d491ef365
StasSaltykov <stasyanych92@gmail.com>
2022-09-08 18:16:05 +0300

    hw# 24 fix

--- src/telran/time/NextFriday13.java
+++ src/telran/time/NextFriday13.java
@@ -10,15 +10,19 @@ public class NextFriday13 implements TemporalAdjuster {
 
 	@Override
 	public Temporal adjustInto(Temporal temporal) {
-		temporal = temporal.plus(1, ChronoUnit.DAYS);
-		while (!isFriday13(temporal)) {
-			temporal = temporal.plus(1, ChronoUnit.DAYS);
+		temporal = adjustTemporal(temporal);
+		while (temporal.get(ChronoField.DAY_OF_WEEK) != DayOfWeek.FRIDAY.ordinal() + 1) {
+			temporal = temporal.plus(1, ChronoUnit.MONTHS);
 		}
 		return temporal;
 	}
-	
-	private boolean isFriday13(Temporal temporal) {
-		return temporal.get(ChronoField.DAY_OF_WEEK) == DayOfWeek.FRIDAY.ordinal() + 1 && temporal.get(ChronoField.DAY_OF_MONTH) == 13;
+
+	private Temporal adjustTemporal(Temporal temporal) {
+
+		if (temporal.get(ChronoField.DAY_OF_MONTH) >= 13) {
+			temporal = temporal.plus(1, ChronoUnit.MONTHS);
+		}
+		return temporal.with(ChronoField.DAY_OF_MONTH, 13);
 	}
 
 }
--- src/telran/time/WorkingDaysAdjuster.java
+++ src/telran/time/WorkingDaysAdjuster.java
@@ -1,19 +1,15 @@
 package telran.time;
 
+import java.time.DayOfWeek;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 
 public class WorkingDaysAdjuster implements TemporalAdjuster {
-	
-	public WorkingDaysAdjuster(int[] daysOff, int nDays) {
-		this.daysOff = daysOff;
-		this.nDays = nDays;
-	}
-	public WorkingDaysAdjuster() {
-		
-	}
+
+	int[] daysOff;
+	int nDays;
 
 	public int[] getDaysOff() {
 		return daysOff;
@@ -30,20 +26,40 @@ public class WorkingDaysAdjuster implements TemporalAdjuster {
 	public void setnDays(int nDays) {
 		this.nDays = nDays;
 	}
-	
-	int[] daysOff;
-	int nDays;
-	
+
+	public WorkingDaysAdjuster(int[] daysOff, int nDays) {
+
+		this.daysOff = daysOff;
+		this.nDays = nDays;
+	}
+
+	public WorkingDaysAdjuster() {
+	}
+
 	@Override
 	public Temporal adjustInto(Temporal temporal) {
-		if (daysOff.length == 7) {
-		    temporal = temporal.plus(0, ChronoUnit.DAYS);
-		} else if (daysOff.length == 0) {
-			temporal = temporal.plus(nDays, ChronoUnit.DAYS);
-		} else if (daysOff.length < 7) {
-			temporal = temporal.plus(nDays + daysOff.length, ChronoUnit.DAYS);
+
+		int count = 0;
+		if (daysOff.length < DayOfWeek.values().length) {
+			while (count != nDays) {
+				temporal = temporal.plus(1, ChronoUnit.DAYS);
+				if (!contains(temporal.get(ChronoField.DAY_OF_WEEK))) {
+					count++;
+				}
+			}
 		}
+
 		return temporal;
+	}
+
+	private boolean contains(int day) {
+		for (int di : daysOff) {
+			if (di == day) {
+				return true;
+			}
+
+		}
+		return false;
 	}
 
 }
