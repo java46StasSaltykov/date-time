276b29037e12aa46195d33bb4d4181f2b0372ac3
StasSaltykov <stasyanych92@gmail.com>
2022-09-10 16:45:35 +0300

    hw #25

---
+++ src/telran/time/application/PrintCalendar.java
@@ -0,0 +1,130 @@
+package telran.time.application;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.YearMonth;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class PrintCalendar {
+
+	public static void main(String[] args) {
+		int monthYear[];
+		try {
+			monthYear = getDayMonthYear(args);
+			printCalendar(monthYear[0], monthYear[1], monthYear[2]);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private static void printCalendar(int month, int year, int startDay) {
+		printTitle(month, year);
+		printWeekDays(startDay);
+		printDays(month, year, startDay);
+	}
+
+	private static void printDays(int month, int year, int startDay) {
+		int column = getFirstColumn(month, year, startDay);
+		printOffset(column);
+		int nDays = getMonthDays(month, year);
+		int nWeekDays = DayOfWeek.values().length;
+		for (int day = 1; day <= nDays; day++) {
+			System.out.printf("%4d", day);
+			column++;
+			if (column == nWeekDays) {
+				column = 0;
+				System.out.println();
+			}
+		}
+	}
+
+	private static int getMonthDays(int month, int year) {
+		YearMonth ym = YearMonth.of(year, month);
+		return ym.lengthOfMonth();
+	}
+
+	private static void printOffset(int column) {
+		System.out.printf("%s", " ".repeat(column * 4));
+	}
+
+	private static int getFirstColumn(int month, int year, int startDay) {
+		LocalDate firstMonthDate = LocalDate.of(year, month, 1);
+		int weekDay = firstMonthDate.getDayOfWeek().getValue();
+		int firstWeekDay = DayOfWeek.values()[startDay - 1].getValue();
+		return weekDay - firstWeekDay >= 0 ? weekDay - firstWeekDay : 7 + weekDay - firstWeekDay;
+	}
+
+	
+	private static void printWeekDays(int startDay) {
+		DayOfWeek dayWeeks[] = DayOfWeek.values();
+		System.out.print("  ");
+		for (DayOfWeek weekDay: dayWeeks) {
+			weekDay = DayOfWeek.of(startDay++);
+			if (startDay > dayWeeks.length) {
+				startDay = dayWeeks[0].getValue();
+			}
+			System.out.printf("%s ", weekDay.getDisplayName(TextStyle.SHORT, Locale.getDefault()));				
+		}
+		System.out.println();
+	}
+
+	private static void printTitle(int month, int year) {
+		Month monthEn = Month.of(month);
+		System.out.printf("%s, %d\n", monthEn.getDisplayName(TextStyle.FULL, 
+				Locale.getDefault()), year);
+	}
+
+	private static int[] getDayMonthYear(String[] args) throws Exception {
+		LocalDate current = LocalDate.now();
+		int[] res = {current.getMonthValue(), current.getYear(), current.getDayOfWeek().getValue()};
+		if (args.length > 0) {
+			res[0] = getMonth(args[0]);
+			if (args.length > 1) {
+				res[1] = getYear(args[1]);
+				if (args.length > 2) {
+					res[2] = getStartDay(args[2]);
+				}
+			}
+		}
+		return res;
+	}
+
+	private static int getStartDay(String startDay) throws Exception {
+		try {
+			int res = DayOfWeek.valueOf(startDay.toUpperCase()).getValue();
+			return res;
+			
+		} catch(IllegalArgumentException e) {
+			throw new Exception("day should be written correctly");
+		}
+	}
+
+	private static int getYear(String yearStr) throws Exception {
+		try {
+			int res = Integer.parseInt(yearStr);
+			if (res <= 0) {
+				throw new Exception("year should be a positive number");
+			}
+			return res;
+		} catch (NumberFormatException e) {
+			throw new Exception("year should be a number");
+		}
+	}
+
+	private static int getMonth(String monthStr) throws Exception {
+		try {
+			int res = Integer.parseInt(monthStr);
+			int nMonths = Month.values().length;
+			if (res < 1 || res > nMonths) {
+				throw new Exception(String.format("month %d is wrong value, "
+						+ "should be in range [1 - %d]", res, nMonths));
+			}
+			return res;
+		} catch (NumberFormatException e) {
+			throw new Exception("month should be a number");
+		}
+	}
+
+}
